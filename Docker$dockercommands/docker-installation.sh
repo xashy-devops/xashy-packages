b885daf4666c0faa98b837e8036a76068437dc5e
jakuma <jakuma998@gmail.com>
2023-06-19 03:23:43 -0400

    added docker folder and installation script

---
+++ Docker$dockercommands/docker-installation.sh
@@ -0,0 +1,8 @@
+### You can make use of the below commands to install docker on your linux server.
+## you can use redhart ec2 instance. 
+#!/bin/bash
+###owner : Xashy Tech 
+#update your linux vm 
+yum update -y 
+# install docker 
+yum install docker -y 
